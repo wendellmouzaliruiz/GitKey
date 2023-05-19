0bc8fb67987fe87dce33e92fea835d951a3a92c3
Wendell Mouzali Ruiz <wendellmouzaliruiz@gmail.com>
2023-05-19 17:41:52 -0300

    Creation of the base project

---
+++ GitKey.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitKey.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
